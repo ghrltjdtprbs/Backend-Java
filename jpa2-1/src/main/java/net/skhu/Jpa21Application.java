273859157ae946fd5d0a65eba9aa192158e102af
최혜미 <ghrltjdtprbs@gmail.com>
2022-12-04 21:09:30 +0900

    Add files via upload

---
+++ jpa2-1/src/main/java/net/skhu/Jpa21Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa21Application.class, args);
+	}
+
+}
