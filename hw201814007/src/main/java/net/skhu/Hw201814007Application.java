3b010d9f0ef6ca1f3f08d75f279d1a21d90585f2
최혜미 <ghrltjdtprbs@gmail.com>
2022-12-06 22:17:24 +0900

    Add files via upload

---
+++ hw201814007/src/main/java/net/skhu/Hw201814007Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw201814007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw201814007Application.class, args);
+	}
+
+}
