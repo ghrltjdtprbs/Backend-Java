3bba95f7629b4515dc87fc9d6847d7fa23be0cbc
최혜미 <ghrltjdtprbs@gmail.com>
2023-03-30 13:46:46 +0900

    수정필요

---
+++ e201814007/src/main/java/net/skhu/E201814007Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E201814007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E201814007Application.class, args);
+	}
+
+}
