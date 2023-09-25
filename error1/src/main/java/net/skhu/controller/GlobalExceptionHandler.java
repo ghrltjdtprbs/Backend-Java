b9d4dd43c9995d618487f73613e725121c2cfc22
최혜미 <ghrltjdtprbs@gmail.com>
2023-09-25 22:50:10 +0900

    Update GlobalExceptionHandler.java

--- error1/src/main/java/net/skhu/controller/GlobalExceptionHandler.java
+++ error1/src/main/java/net/skhu/controller/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
-//
+
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
